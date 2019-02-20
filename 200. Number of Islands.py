6d8aa78224e48a30af952afc5310bea033b43a67
xin <xin.miao.uta@gmail.com>
2019-02-19 22:33:14 -0600

    Update 200. Number of Islands.py

--- 200. Number of Islands.py
+++ 200. Number of Islands.py
@@ -4,6 +4,41 @@ class Solution(object):
     :type grid: List[List[str]]
     :rtype: int
     """
+    if not grid or not grid[0]:
+        return 0
+
+    def dfs(grid,i,j):
+      grid[i][j]='0'
+      if i>0 and grid[i-1][j]=='1':
+        dfs(grid,i-1,j)
+      if i<len(grid)-1 and grid[i+1][j]=='1':
+        dfs(grid,i+1,j)
+      if j>0 and grid[i][j-1]=='1':
+        dfs(grid,i,j-1)
+      if j<len(grid[0])-1 and grid[i][j+1]=='1':
+        dfs(grid,i,j+1)
+
+
+    nums_island = 0
+    w, h = len(grid), len(grid[0])
+    for m in range(w):
+      for n in range(h):
+        if grid[m][n] == '1':
+            dfs(grid,m,n)
+            nums_island += 1
+
+    return nums_island
+
+
+
+
+'''
+class Solution(object):
+  def numIslands(self, grid):
+    """
+    :type grid: List[List[str]]
+    :rtype: int
+    """
     if grid==[]:
         return 0
     def dfs(i, j, l):
@@ -33,3 +68,4 @@ class Solution(object):
     return nums_island
 
   ##but got Time Limit Exceeded###
+'''
